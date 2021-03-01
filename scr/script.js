561a3c2903131f70e73e898f1f9e745d9212277d
Lucifer <290168673@qq.com>
2021-03-01 23:16:28 +0900

    Update script.js

--- scr/script.js
+++ scr/script.js
@@ -3,23 +3,17 @@ $(function(){
 })
 
 $('#collapseExample').on('show.bs.collapse', function () {
-    // do something…
-    // $("#collapseExample1").collapse('hide');
-    // $("#collapseExample2").collapse('hide');
     $("div.collapse").collapse("hide")
   })
 
 $('#collapseExample1').on('show.bs.collapse', function () {
-    // do something…
-    // $("#collapseExample").collapse('hide');
-    // $("#collapseExample2").collapse('hide');
     $("div.collapse").collapse("hide")
   })
 
 $('#collapseExample2').on('show.bs.collapse', function () {
-    // do something…
-    // $("#collapseExample1").collapse('hide');
-    // $("#collapseExample").collapse('hide');
     $("div.collapse").collapse("hide")
   })
 
+$('#collapseExample3').on('show.bs.collapse', function () {
+    $("div.collapse").collapse("hide")
+  })
