62d12d4cbb8795784f7a67e42b0aa68880195c3d
Lucifer <290168673@qq.com>
2021-03-01 22:40:53 +0900

    Update script.js

--- scr/script.js
+++ scr/script.js
@@ -4,18 +4,22 @@ $(function(){
 
 $('#collapseExample').on('show.bs.collapse', function () {
     // do something…
-    $("#collapseExample1").collapse('hide');
-    $("#collapseExample2").collapse('hide');
+    // $("#collapseExample1").collapse('hide');
+    // $("#collapseExample2").collapse('hide');
+    $("div.collapse").collapse("hide")
   })
 
 $('#collapseExample1').on('show.bs.collapse', function () {
     // do something…
-    $("#collapseExample").collapse('hide');
-    $("#collapseExample2").collapse('hide');
+    // $("#collapseExample").collapse('hide');
+    // $("#collapseExample2").collapse('hide');
+    $("div.collapse").collapse("hide")
   })
 
 $('#collapseExample2').on('show.bs.collapse', function () {
     // do something…
-    $("#collapseExample1").collapse('hide');
-    $("#collapseExample").collapse('hide');
+    // $("#collapseExample1").collapse('hide');
+    // $("#collapseExample").collapse('hide');
+    $("div.collapse").collapse("hide")
   })
+
