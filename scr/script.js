e0598a1d9ff8809a6c8ae4e52ba2346b8cacb9e8
Lucifer <290168673@qq.com>
2021-03-09 16:54:17 +0900

    Update script.js

--- scr/script.js
+++ scr/script.js
@@ -1,22 +1,22 @@
 $(function () {
   console.log("loaded")
-})
+});
 
 $('#collapseExample').on('show.bs.collapse', function () {
   $("div.collapse").collapse("hide")
-})
+});
 
 $('#collapseExample1').on('show.bs.collapse', function () {
   $("div.collapse").collapse("hide")
-})
+});
 
 $('#collapseExample2').on('show.bs.collapse', function () {
   $("div.collapse").collapse("hide")
-})
+});
 
 $('#collapseExample3').on('show.bs.collapse', function () {
   $("div.collapse").collapse("hide")
-})
+});
 
 //下拉框查询组件点击查询栏时不关闭下拉框
 $("body").on('click', '[data-stopPropagation]', function (e) {
@@ -37,11 +37,12 @@ $(window).resize(function () {
 //当屏幕小于615时添加一个属性，大于的时候删除属性
 function changeClass() {
   let ww = $(window).width();
-  if (ww > 615) {
+  if (ww > 560) {
     for (var x = 0; x < $("div.btn-group").length; x++) {
-      $("div.btn-group")[x].classList.remove('dropright')
+      $("div.btn-group")[x].classList.remove("dropright");
+      $("div.st-btn-container button")[x].removeAttribute("data-offset");
     }
-  } else if (ww < 615) {
+  } else if (ww < 561) {
     for (var x = 0; x < $("div.btn-group").length; x++) {
       if ($("div.btn-group")[x].className.indexOf("dropright") === -1) {
         $("div.btn-group")[x].classList.add("dropright")
